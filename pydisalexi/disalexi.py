258f2c37cae9571f31c3b616d912970cb196107c
bucricket <bucricket@gmail.com>
2017-06-01 08:49:57 -0400

    Fixed silly mistake

--- pydisalexi/disalexi.py
+++ pydisalexi/disalexi.py
@@ -388,7 +388,7 @@ class disALEXI(object):
 
         #-------------get Landsat information-----------
 
-        ls = GeoTIFF(os.path.join(self.landsatSR,'temp','*lstSharp.tiff' % productID))
+        ls = GeoTIFF(os.path.join(self.landsatSR,'temp','%s_lstSharp.tiff' % productID))
         solZen = self.meta.SUN_ELEVATION
 #        nsamples = int(self.meta.REFLECTIVE_SAMPLES)
 #        nlines = int(self.meta.REFLECTIVE_LINES)
