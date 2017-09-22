bd007eb475bf15fce181bea979d3fa874f5ee21b
bucricket <bucricket@gmail.com>
2017-09-22 07:59:05 -0400

    Commented for testing purposes

--- pydisalexi/pydisalexi_usda.py
+++ pydisalexi/pydisalexi_usda.py
@@ -87,17 +87,18 @@ def main():
                    
             #============Run DisALEXI in parallel======================================
             dd = disALEXI(filepath,dt,isUSA)
-            dd.runDisALEXI(0,0,ALEXIgeodict,0)
-            nsamples = ll.nrow
-            nlines = ll.ncol
-            print 'Running disALEXI...'
-            r = Parallel(n_jobs=njobs, verbose=5)(delayed(dd.runDisALEXI)(xStart,yStart,ALEXIgeodict,0) for xStart in range(0,nsamples,200) for yStart in range(0,nlines,200))            
-            
-            # =================merge Ta files============================================
-            print 'merging Ta files...'            
-            intFile = os.path.join(resultsBase,scene,'Taxxxxx.tif')
-            cmd = 'gdal_merge.py -o %s %s' % (intFile,os.path.join(resultsBase,scene,'Ta*'))
-            buildvrt(cmd)
+            #===COMMENTED FOR TESTING ONLY===================
+#            dd.runDisALEXI(0,0,ALEXIgeodict,0)
+#            nsamples = ll.nrow
+#            nlines = ll.ncol
+#            print 'Running disALEXI...'
+#            r = Parallel(n_jobs=njobs, verbose=5)(delayed(dd.runDisALEXI)(xStart,yStart,ALEXIgeodict,0) for xStart in range(0,nsamples,200) for yStart in range(0,nlines,200))            
+#            
+#            # =================merge Ta files============================================
+#            print 'merging Ta files...'            
+#            intFile = os.path.join(resultsBase,scene,'Taxxxxx.tif')
+#            cmd = 'gdal_merge.py -o %s %s' % (intFile,os.path.join(resultsBase,scene,'Ta*'))
+#            buildvrt(cmd)
             
             # =================run TSEB one last time in parallel=======================
             dd.runDisALEXI(0,0,ALEXIgeodict,1)
