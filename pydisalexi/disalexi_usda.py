d9d9d18f913f6121ed542c43a9fb6a5c18bc8f8d
bucricket <bucricket@gmail.com>
2018-02-06 21:16:48 -0500

    Trying something else

--- pydisalexi/disalexi_usda.py
+++ pydisalexi/disalexi_usda.py
@@ -356,8 +356,8 @@ class disALEXI(object):
 #        group = etDF['et'].groupby(etDF['ID'])
         etDF = pd.DataFrame(etDict)
         group = etDF.groupby(etDF['ID'])
-        valMean = group.mean()
-#        valMean = group.transform('mean')
+#        valMean = group.mean()
+        valMean = group.transform('mean')
 #        outData = np.zeros(et_masked.size)
         outData = np.zeros(et.shape)
         for i in range(valMean.shape[0]):
