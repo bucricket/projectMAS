fbe30e4f1a50aac59c212ba86d2e3975debf4b96
bucricket <bucricket@gmail.com>
2018-03-02 20:18:08 -0500

    Lowering the image subset

--- pydisalexi/pydisalexi_usda.py
+++ pydisalexi/pydisalexi_usda.py
@@ -55,7 +55,7 @@ def main():
     sat = args.sat
     start_date = args.start_date
     end_date = args.end_date
-    subsetSize = 400
+    subsetSize = 300
     base = os.getcwd()
     cacheDir = os.path.abspath(os.path.join(base,os.pardir,"SATELLITE_DATA"))
    
